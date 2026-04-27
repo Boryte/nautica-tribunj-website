76c9954df0d9686f82403b202ff9bae8f1f69f06
Boryte <91828774+Boryte@users.noreply.github.com>
2026-04-27 14:20:59 +0200

    Delete server-dist directory

--- server-dist/apps/api/src/scripts/seed.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const migrate_1 = require("../migrate");
-const seed_1 = require("../seed");
-(0, migrate_1.runMigrations)();
-(0, seed_1.seedDatabase)();
